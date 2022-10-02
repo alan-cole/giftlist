134706eb79cca6128d5023b87516e6fd5a454124
Alan Cole <alan@salsadigital.com.au>
2022-10-02 23:58:34 +1100

    Fix api calls on browser.

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,6 @@
 import { Connector } from './connector.js'
 
-const host = import.meta.env.VITE_GL_API || window.location.origin
+const host = (typeof window === 'undefined') ? import.meta.env.VITE_GL_API : window.location.origin
 const token = localStorage.getItem('token') || ''
 
 export default new Connector(host, token)
