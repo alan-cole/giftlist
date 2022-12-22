951010ec39514d3aa810e10f9c5f1d774053f4c5
Alan Cole <alan@salsadigital.com.au>
2022-12-23 00:30:00 +1100

    Added BuyButton component.

--- src/lib/api.js
+++ src/lib/api.js
@@ -1,6 +1,7 @@
 import { Connector } from './connector.js'
 
-const host = (typeof window === 'undefined') ? import.meta.env.VITE_GL_API : window.location.origin
+// const host = (typeof window === 'undefined') ? import.meta.env.VITE_GL_API : window.location.origin
+const host = import.meta.env.VITE_GL_API
 const token = localStorage.getItem('token') || ''
 
 export default new Connector(host, token)
