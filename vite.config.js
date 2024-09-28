6c8bfae978b58d989f07abd44ec7c87ebecbbc65
Alan Cole <alan.d.m.cole@gmail.com>
2024-09-29 00:27:42 +1000

    Update packages.

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: 'modern' // or "modern"
+      }
+    }
+  },
   plugins: [vue()],
   resolve: {
     alias: {
